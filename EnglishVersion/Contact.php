0e158a0632fc88d7dc95d13099d9885071be78f4
abolo <abolfazle0915501@gmail.com>
2024-10-20 22:13:12 +0200

    contact form

--- EnglishVersion/Contact.php
+++ EnglishVersion/Contact.php
@@ -1,31 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../style.css">
 </head>
-<body>
-<?php
-      include_once("../phpLibrary/MyLibrary.php");
-NavigationBar("Contact");
-?>
 
-<?php
-    if(!isset($_GET["name"])){
-        ("filed ". $_GET["name"] . "must be completed.");
-    }elseif(!isset($_GET["LastName"])){
-        print("filed ". $_GET["LastName"] . "must be completed.");
-    }elseif(!isset($_GET["Email"])){
-        print("filed ". $_GET["Email"] . "must be completed.");
+<body>
+    <?php
+    include_once("../phpLibrary/MyLibrary.php");
+    NavigationBar("Contact");
+    ?>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["submit"])) {
+        submitForm();
     }
-?>
-<form action="">
-    <input type="text" placeholder="First name" name="UserName">
-    <input type="text" placeholder="Last name" lastName="LastName">
-    <input type="email" placeholder="Email" Email="Email">
-    <input type="submit">
-</form>
+
+    function submitForm()
+    {
+        if (empty($_GET["UserName"])) {
+            print("Form cant be submitted.");
+        } elseif (empty($_GET["LastName"])) {
+            print("Field in submision, Last name filled cant be empty");
+        } elseif (empty($_GET["Email"])) {
+            print("Field in submision, Email filled cant be empty");
+        } else {
+            print("Form submitted successfully");
+        }
+    };
+
+    ?>
+    <div class="form-location">
+        <div class="container-form">
+            <h3>Contact us</h3>
+            <form action="" method="GET">
+                <input type="text" placeholder="First name" name="UserName">
+                <input type="text" placeholder="Last name" name="LastName">
+                <input type="email" placeholder="Email" name="Email">
+
+                <a href="#">Forgotten password</a>
+                <button type="submit" name="submit">submit</button>
+                <!-- <input type="submit" name="submit"> -->
+            </form>
+        </div>
+    </div>
+
+
 </body>
+
 </html>
