fe1fd9746ffeb512df559c9f98ccf1b6ab3befff
abolo <abolfazle0915501@gmail.com>
2024-10-22 21:18:28 +0200

    erf

--- EnglishVersion/Contact.php
+++ EnglishVersion/Contact.php
@@ -21,24 +21,40 @@
 
     function submitForm()
     {
-        if (isset($_GET["UserName"])) {
-            if ($_GET["UserName"] == "") {
-                print("Form cant be submitted.");
+        // if (isset($_GET["UserName"])) {
+        //     if ($_GET["UserName"] == "") {
+        //         print("Form cant be submitted.");
+        //     }
+        // } elseif (isset($_GET["LastName"])) {
+        //     if ($_GET["LastName"] == "") {
+        //         print("Field in submision, Last name filled cant be empty");
+        //     }
+        // } elseif (isset($_GET["Email"])) {
+        //     if ($_GET["LastName"] == "") {
+        //         print("Field in submision, Email filled cant be empty");
+        //     }
+        // } else {
+        //     print("Form submitted successfully");
+        // }
+
+        $inputs = array(
+            "Key1" => "UserName",
+            "Key2" => "LastName",
+            "Key3" => "Email"
+        );
+        foreach ($inputs as $key => $value) {
+            if (isset($_GET[$value])) {
+                if ($_GET[$value] == "") {
+                    print("Please filled up all the inputs! form submission failed");
+                    break;
+                } else {
+                    print("Form submitted successfully!");
+                    break;
+                }
             }
-        } elseif (isset($_GET["LastName"])) {
-            if ($_GET["LastName"] == "") {
-                print("Field in submision, Last name filled cant be empty");
-            }
-        } elseif (isset($_GET["Email"])) {
-            if ($_GET["LastName"] == "") {
-                print("Field in submision, Email filled cant be empty");
-            }
-        } else {
-            print("Form submitted successfully");
         }
     };
 
-    
 
     ?>
     <div class="form-location">
@@ -49,13 +65,11 @@
                 <input type="text" placeholder="Last name" name="LastName">
                 <input type="email" placeholder="Email" name="Email">
 
-                <a href="#">Forgotten password</a>
-
+                <a href="#" class="Forgotten-password">Forgotten password</a>
 
                 <input type="submit" name="submit">
             </form>
             <a href="Contact.php">Reset the form and search</a>
-            <a href="Contact.php?UserName=Abolo">Abolo</a>
         </div>
     </div>
 
--- EnglishVersion/Home.php
+++ EnglishVersion/Home.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../style.css">
 </head>
+
 <body>
-<?php
-              include_once("../phpLibrary/MyLibrary.php");
-NavigationBar("Home");
-?>
+    <?php
+    include_once("../phpLibrary/MyLibrary.php");
+    NavigationBar("Home");
+    ?>
 
 </body>
+
 </html>
