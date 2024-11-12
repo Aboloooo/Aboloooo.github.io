ac90c6b3f444a8ac4b05464c8d390c2e558cc2e5
Aboloooo <abolfazle0915501@gmail.com>
2024-11-12 15:19:03 +0100

    asfr

--- EnglishVersion/Login.php
+++ EnglishVersion/Login.php
@@ -17,30 +17,33 @@
     $accounts = "../DataBases/Client_DataBase.csv";
 
     //check if all the filleds are filled up
-    if (isset($_POST["username"], $_POST["password"])) {
-        $usernameInput = $_POST["username"];
-        $passwordInput = $_POST["password"];
-        $SucessfulLogin = false;    //Flags are really important
+   if(isset($_POST["username"] , $_POST["password"])){
+    $usernameInput = $_POST["username"];
+    $passwordInput = $_POST["password"];
+    $sucessfullLogin = false;    //Flags are really important
+    
+    //check if the database exists
+    if(file_exists($accounts)){
+        //Read the database line by line and separate the username and pass from eachother using explode function
+        $openFile = fopen($accounts,"r");
 
-        //check if the database exists
-        if (file_exists($accounts)) {
-            $OpenedFile = fopen($accounts, "r");
-            //Read the database line by line and separate the username and pass from eachother using explode function
-            while (($line = fgets($OpenedFile)) !== false) {
-                list($username, $password) = explode(" => ", $line);
-                // check if user has an account already
-                if ($usernameInput == $username && $passwordInput == $password) {
-                    print("Sucessful login!");
-                    $SucessfulLogin = true;
-                    break;
-                }
-            }
-        }
+        
+        while(($line = fgets($openFile))!==false){
+            list($username , $password) = explode(" => " , $line);
+          // check if user has an account already
+          if($username== $usernameInput && $password == $passwordInput){
+            print("Login successful!");
+            $sucessfullLogin = true;
+            break;
+          };
 
-        if (!$SucessfulLogin) {
-            print("Login failed! please try again.");
-        }
+        };
     }
+    if(!$sucessfullLogin){
+        print("Login failled!");
+       }
+   }
+             
     ?>
 
     <div>
