92518f8815b459fe801782e8c365472a79427304
abolo <abolfazle0915501@gmail.com>
2024-11-23 16:50:48 +0100

    file handling issue

--- EnglishVersion/About.php
+++ EnglishVersion/About.php
@@ -5,25 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<!-- bank of icon  https://boxicons.com/  -->
-<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <!-- bank of icon  https://boxicons.com/  -->
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <link rel="stylesheet" href="../style.css? <?= time(); ?>">
     <script>
-
         window.onload = function() {
             document.getElementById("btn").addEventListener("click", getLocation);
-        };    
-            function getLocation (){
-            navigator.geolocation.getCurrentPosition(function(position){
+        };
+
+        function getLocation() {
+            navigator.geolocation.getCurrentPosition(function(position) {
                 const latitude = position.coords.latitude
                 const longitude = position.coords.longitude
-                let output = document.getElementById("output").toLowerCase;
-                 
-                if(output() == "home"){
-                    output.innerHTML = latitude + "<br>" + longitude
-                }else{
-                    alert("Error")
-                }    
+                let input = document.getElementById("input").value;
+                let inputCorrection = input.toLowerCase();
+                let output = document.getElementById("output");
+
+                if (inputCorrection == "home") {
+                    output.innerHTML = "latitude: " + latitude + "<br>" + "longitude: " + longitude
+                } else {
+                    alert("Please try again!")
+                }
             })
         }
     </script>
@@ -35,14 +37,14 @@
     NavigationBarE("About");
     ?>
     <label for="">Write Home</label>
-    <input type="text" placeholder="write home here">
+    <input type="text" id="input" placeholder="write home here">
     <button id="btn">submit</button>
     <p id="output"></p>
     <!-- the following function will create a end bar in the end of the content of a webpage -->
     <?php
     EndBar()
     ?>
-    
+
 </body>
 
 </html>
--- EnglishVersion/AddingNewProduct.php
+++ EnglishVersion/AddingNewProduct.php
@@ -16,22 +16,58 @@
     NavigationBarE("");
     ?>
 
+    <?php
+    $completedProductDetails = false;
+
+    if (isset($_POST["submit"])) {
+        $productName = $_POST["productName"];
+        $productDescription = $_POST["productDescription"];
+        $productPrice = $_POST["productPrice"];
+        $productSize = $_POST["productSize"];
+        $productGender = $_POST["productGender"];
+
+
+        // now checking if all the neccesary inputs are filled up
+        if (!isset($productName, $productDescription, $productPrice,  $productSize, $productGender, $productImg)) {
+            die("Error: One or more product details are missing.");
+            $completedProductDetails = false;
+        } else {
+            $completedProductDetails = true;
+        }
+
+        if ($completedProductDetails) {
+            // $productName, $productDescription, $productPrice,  $productSize, $productGender, $productImg
+            $productBank =  fopen("../DataBases/Products.csv", "a");
+            $NewProduct = [$productName, $productDescription, $productPrice,  $productSize, $productGender, $productImg];
+
+            fputcsv($productBank, $NewProduct);
+        }
+    }
+    ?>
+
     <div class="container-new-product">
         <div class="img-new-product">
-            <input type="file" name="file">
+            <form action="" method="POST">
+                <input type="file" name="productImg">
+            </form>
         </div>
         <div class="detail-new-product">
-            <form action="" class="form">
+            <form action="" method="POST" class="form">
                 <label for="">Product name</label>
-                <input type="text">
+                <input type="text" name="productName">
+
                 <label for="">Description</label>
-                <input type="text">
+                <input type="text" name="productDescription">
+
                 <label for="">Price</label>
-                <input type="number">
+                <input type="number" name="productPrice">
+
                 <label for="">Sizes availible</label>
-                <input type="text">
+                <input type="text" name="productSize">
+
                 <label for="">Gender usage</label>
-                <input type="text">
+                <input type="text" name="productGender">
+
                 <input type="submit" value="submit" name="submit">
             </form>
         </div>
